2a0ce322f61bbe28bfec219cd7b5ca5835002fe1
Nicholas Hubbard <nicholashubbard@posteo.net>
2025-01-15 09:32:13 -0500

    more theorems :)

--- misc/Tree.v
+++ misc/Tree.v
@@ -30,21 +30,69 @@ Fixpoint count (A : Set) (t : binary_tree A) : nat :=
     | Node _ _ lchild rchild => 1 + count A lchild + count A rchild
   end.
 
-Theorem count_length_traversal:
-  forall (A : Set) (t : binary_tree A), count A t = length (traverse_preorder A t).
+Lemma length_cons : forall (A : Set) (l : list A) a, length (a :: l) = S (length l).
 Proof.
-  intros.
+  intros A l a.
+  simpl.
+  trivial.
+Qed.
+
+Lemma length_cons_nil : forall (A : Set) (a : A), length (a :: nil) = 1.
+Proof.
+  auto.
+Qed.
+
+Lemma S_plus_1 : forall n : nat, S n = n + 1.
+Proof.
+  intros n.
+  induction n as [| n' IHn].
+  - trivial.
+  - simpl.
+    auto.
+Qed.
+
+Theorem eq_length_traversals:
+  forall (A : Set) (t : binary_tree A),
+    length (traverse_preorder A t) = length (traverse_inorder A t) /\
+    length (traverse_inorder  A t) = length (traverse_postorder A t).
+Proof.
+  intros A t.
+  split.
   induction t.
-  { simpl.
+  - simpl.
     trivial.
-  }
-  {
+  - simpl.
+    repeat rewrite length_app.
+    rewrite length_cons.
+    rewrite IHt1.
+    rewrite IHt2.
+    auto.
+  - induction t.
     simpl.
+    trivial.
+    simpl.
+    repeat rewrite length_app.
+    rewrite IHt1.
+    rewrite <- IHt2.
+    rewrite length_cons.
+    rewrite length_cons_nil.
+    rewrite S_plus_1.
+    trivial.
+Qed.
+
+Theorem count_length_traversal:
+  forall (A : Set) (t : binary_tree A),
+    count A t = length (traverse_preorder A t).
+Proof.
+  intros A t.
+  induction t.
+  - simpl.
+    trivial.
+  - simpl.
     rewrite IHt1.
     rewrite IHt2.
     rewrite length_app.
     trivial.
-  }
 Qed.
 
 End Binary_Tree.
